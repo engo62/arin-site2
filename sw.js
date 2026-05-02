a7c81b986bcc0695158ebf942b0d3edb555de29a
engo62 <eyoltay882@gmail.com>
2026-05-02 18:36:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,30 @@
-self.addEventListener("install", e => {
-e.waitUntil(
-caches.open("arin-site").then(cache => {
-return cache.addAll([
+const CACHE_NAME="arin-app-v1";
+
+const FILES=[
 "./",
 "./index.html",
+"./icon.png",
 "./sarki.mp3",
-"./icon.png"
-]);
-})
+"./foto1.jpg",
+"./foto2.jpg",
+"./foto3.jpg",
+"./foto4.jpg",
+"./foto5.jpg",
+"./foto6.jpg",
+"./foto7.jpg",
+"./foto8.jpg",
+"./foto9.jpg",
+"./foto10.jpg"
+];
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE_NAME).then(c=>c.addAll(FILES))
 );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch",e=>{
 e.respondWith(
-caches.match(e.request).then(res => res || fetch(e.request))
+caches.match(e.request).then(r=>r||fetch(e.request))
 );
 });
